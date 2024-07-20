43fa31bc7bec9ac302861c73975bf2b2c10d5a10
yhgong <kkle1994@outlook.com>
2024-07-21 01:56:46 +0800

    修复部分代码Warning，移除不再需要的代码

--- HappyLife/HappyLife.cs
+++ HappyLife/HappyLife.cs
@@ -21,7 +21,7 @@ namespace HappyLife
     {
         public static bool IsAutoCollecting = false;
         public static List<RenderInfo> MonthlyInfos = new List<RenderInfo>();
-        public static HappyLife Instance;
+        public static HappyLife? Instance;
         public static bool EventPatchLoaded = false;
         public override void Initialize()
         {
@@ -49,7 +49,7 @@ namespace HappyLife
                 if (DomainManager.Mod.GetSetting(modId.ToString(), field, ref result))
                     return result;
             }
-            return null;
+            return string.Empty;
         }
 
         public static bool GetBoolSettings(string field)
@@ -88,61 +88,6 @@ namespace HappyLife
             }
         }
 
-        [HarmonyPatch(typeof(WorldDomain), "OnLoadWorld")]
-        public class EventOutput
-        {
-            public static bool Prefix()
-            {
-                //if (EventPatchLoaded)
-                //    return true;
-                //if (GetBoolSettings("UnlimitedConfess"))
-                //{
-                //    Harmony harmony = new Harmony("Taiwuhentai event");
-                //    harmony.PatchAll(typeof(HappyLifeTest.OnOption9VisibleCheckPatch));
-                //}
-
-
-                //var fields = typeof(TaiwuEventDomain).GetFields(BindingFlags.Static | BindingFlags.NonPublic);
-                //var properties = typeof(TaiwuEventDomain).GetProperties(BindingFlags.Static | BindingFlags.NonPublic);
-                //var method = DomainManager.TaiwuEvent.GetType().GetField("_managerArray", BindingFlags.Static & BindingFlags.NonPublic);
-                //var managerArray = DomainManager.TaiwuEvent.GetType().GetField("_managerArray", BindingFlags.Static | BindingFlags.NonPublic).GetValue(DomainManager.TaiwuEvent) as EventManagerBase[];
-                //foreach (var manager in managerArray)
-                //{
-                //    if (manager == null)
-                //        continue;
-                //    var eventDictionary = manager.GetType().GetField("_eventDictionary", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(manager) as Dictionary<string, TaiwuEvent>;
-
-
-                //    var filename = $"EventData_{manager.GetType()}.csv";
-                //    if (File.Exists(filename))
-                //    {
-                //        File.Delete(filename);
-                //    }
-                //    var lines = new List<string>();
-                //    var headerLine = "Guid,OptionKey,Text,Package";
-                //    lines.Add(headerLine);
-                //    foreach (var item in eventDictionary.Values)
-                //    {
-                //        var line = $"{item.EventGuid},{item.EventConfig.EscOptionKey},{item.EventConfig.EventContent.Replace(",", "<:comma>").Replace("\n", "<:lineend>")},{item.EventConfig.Package.ToString()}";
-                //        lines.Add(line);
-                //    }
-                //    File.WriteAllLines(filename, lines);
-                //}
-
-                return true;
-            }
-        }
-
-        //[HarmonyPatch(typeof(RelationType), "AllowAddingRelation")]
-        //public class AllowAddingRelationPatch
-        //{
-        //    public static void Postfix(int charId, ref bool __result)
-        //    {
-        //        if (GetBoolSettings("AllowAllRelation") && charId == DomainManager.Taiwu.GetTaiwuCharId())
-        //            __result = true;
-        //    }
-        //}
-
         [HarmonyPatch(typeof(CharacterDomain), "CalcFavorabilityDelta")]
         public class CalcFavorabilityDeltaPatch
         {
@@ -153,13 +98,12 @@ namespace HappyLife
                     var trace = new StackTrace();
                     if (__result < 0 && GetBoolSettings("CancelMonthlyEventFavorReduce"))
                     {
-                        if (trace.GetFrames().Exist(f => f.GetMethod().Name == "ApplyInitialChangesForTaiwu" || f.GetMethod().Name == "MakeSecretInformationBroadcast"))
+                        if (trace.GetFrames().Exist(f => f.GetMethod()!.Name == "ApplyInitialChangesForTaiwu" || f.GetMethod()!.Name == "MakeSecretInformationBroadcast"))
                         {
                             __result = 0;
                             return;
                         }
                     }
-
 
                     var multiple = GetIntSettings("FavorabilityMultiple");
                     if (__result > 0)
@@ -168,14 +112,12 @@ namespace HappyLife
             }
         }
 
-
-
         [HarmonyPatch(typeof(CombatDomain), "ReduceWeaponDurability")]
         public class NoWeaponDurablilityPatch
         {
             public static bool Prefix(ref int reduceValue)
             {
-                Random random = new Random();
+                var random = new Random();
                 var rate = GetIntSettings("WeaponDurabilityRate");
                 reduceValue = random.Next(1, 100) < rate ? 1 : 0;
                 return true;
@@ -197,13 +139,6 @@ namespace HappyLife
         [HarmonyPatch(typeof(WorldDomain), nameof(WorldDomain.GetCombatDifficulty))]
         public class GetCombatDifficultyPatch
         {
-            //static Dictionary<string, byte> DifficultyDictionary = new Dictionary<string, byte>()
-            //{
-            //    { "简单", 0 },
-            //    { "正常", 1 },
-            //    { "困难", 2 },
-            //    { "极难", 3 },
-            //};
             public static void Postfix(ref byte __result)
             {
                 if (GetIntSettings("CombatDiffcultyOverwrite") != 0)
@@ -212,65 +147,6 @@ namespace HappyLife
                 }
             }
         }
-
-        //[HarmonyPatch(typeof(BuildingDomain), "CanBuild")]
-        //public class UnlimitedDependBuilding
-        //{
-        //    public static void Postfix(ref bool __result)
-        //    {
-        //        if (GetBoolSettings("UnlimitedDependBuilding"))
-        //            __result = true;
-        //    }
-        //}
-
-        //[HarmonyPatch(typeof(BuildingDomain), "SerialUpdate")]
-        //public class SerialUpdatePatch
-        //{
-        //    public static void Postfix(BuildingDomain __instance, DataContext context)
-        //    {
-        //        HappyLife.IsAutoCollecting = true;
-        //        if (GetBoolSettings("QuickCollectBuildingEarn"))
-        //            __instance.QuickCollectBuildingEarn(context);
-
-        //        if (GetBoolSettings("QuickRecruitPeople"))
-        //        {
-        //            __instance.QuickRecruitPeople(context);
-        //            var info = new RenderInfo(117, $"建筑已被自动重新安排人员。");
-        //            MonthlyInfos.Add(info);
-        //        }
-        //        HappyLife.IsAutoCollecting = false;
-        //    }
-        //}
-
-        //[HarmonyPatch(typeof(BuildingDomain), "AcceptBuildingBlockCollectEarning")]
-        //public class AcceptBuildingBlockCollectEarningPatch
-        //{
-        //    public static void Postfix(BuildingDomain __instance, DataContext context, BuildingBlockKey key)
-        //    {
-        //        if (HappyLife.IsAutoCollecting)
-        //        {
-        //            var collection = DomainManager.World.GetMonthlyNotificationCollection();
-        //            var data = __instance.GetBuildingBlockData(key);
-        //            MapAreaData element_Areas = DomainManager.Map.GetElement_Areas(key.AreaId);
-        //            int settlementIndex = DomainManager.Map.GetElement_Areas(key.AreaId).GetSettlementIndex(key.BlockId);
-        //            short settlementId = element_Areas.SettlementInfos[settlementIndex].SettlementId;
-        //            collection.AddBuildingIncome(settlementId, data.TemplateId);
-        //        }
-        //    }
-        //}
-
-        //[HarmonyPatch(typeof(MonthlyNotificationCollection), "GetRenderInfos")]
-        //public class MonthlyNotificationCollectionPatch
-        //{
-        //    public static void Postfix(List<RenderInfo> renderInfos)
-        //    {
-        //        renderInfos.AddRange(MonthlyInfos);
-        //        MonthlyInfos.Clear();
-        //    }
-        //}
-        //GetRenderInfos
-        //QuickCollectBuildingEarn
-        //QuickRecruitPeople
 
         [HarmonyPatch(typeof(TaiwuDomain), "CalcBuildingSpaceLimit")]
         public class IncreaseBuildingMaxCount
